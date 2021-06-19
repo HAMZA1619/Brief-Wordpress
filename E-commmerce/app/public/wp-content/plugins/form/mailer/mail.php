3be387aca071a2f33c83249112437d98bfcf12d5
HAMZA1619 <76818648+HAMZA1619@users.noreply.github.com>
2021-06-19 15:04:21 +0100

    Update mail.php

--- E-commmerce/app/public/wp-content/plugins/form/mailer/mail.php
+++ E-commmerce/app/public/wp-content/plugins/form/mailer/mail.php
@@ -16,8 +16,8 @@ $mail = new PHPMailer();
    $mail->isSMTP();                                            //Send using SMTP
    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-   $mail->Username   = 'hamzaelg32@gmail.com';                     //SMTP username
-   $mail->Password   = 'hamza0651018901';                               //SMTP password
+   $mail->Username   = '';                     //SMTP username
+   $mail->Password   = '';                               //SMTP password
    $mail->SMTPSecure = 'ssl';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
    $mail->Port       = 465; 
    
